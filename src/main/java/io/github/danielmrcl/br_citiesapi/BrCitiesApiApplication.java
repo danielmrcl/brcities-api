bdb88df8e0615b0f6108d84b67b1378bc49144e3
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-04-25 18:05:55 -0300

    projeto inicializado

---
+++ src/main/java/io/github/danielmrcl/br_citiesapi/BrCitiesApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.danielmrcl.br_citiesapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrCitiesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrCitiesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/danielmrcl/br_citiesapi/BrCitiesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.danielmrcl.br_citiesapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrCitiesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
