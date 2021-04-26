95c0822c42014b966e0862f168088536045a7185
Daniel Marcl <49128637+danielmrcl@users.noreply.github.com>
2021-04-26 01:31:42 -0300

    consertado o nome de staties para states

--- src/main/java/io/github/danielmrcl/br_citiesapi/resources/StateResource.java
+++ src/main/java/io/github/danielmrcl/br_citiesapi/resources/StateResource.java
@@ -11,16 +11,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/staties")
+@RequestMapping("/states")
 public class StateResource {
     @Autowired
     private StateRepository repository;
 
     @GetMapping
-    public Page<State> getAllStaties(Pageable page) {
+    public Page<State> getAllStates(Pageable page) {
         return repository.findAll(page);
     }
 
